db610d8c42d34f3c87bec2516519982b253a8067
Serezha <volos11223344@yandex.ru>
2024-12-09 22:13:03 +0300

    тип ingredientBox

---
+++ src/components/Ingredient/Ingredient.tsx
@@ -0,0 +1,69 @@
+import React from 'react';
+import { ItemConstructor } from '../../utils/types.tsx';
+import IngredientCss from './Ingredient.module.css';
+import { useDrag } from 'react-dnd';
+import { CurrencyIcon, Counter } from '@ya.praktikum/react-developer-burger-ui-components';
+
+interface Item {
+	_id: string;
+}
+
+interface IngredientProps {
+	dataStore: ItemConstructor[];
+	item: Item;
+	isModalOpen: boolean;
+	image: string;
+	name: string;
+	price: number;
+	openModal: () => void;
+	_id: string;
+}
+
+const Ingredient: React.FC<IngredientProps> = (props): React.JSX.Element => {
+	const { dataStore, image, name, price, item } = props;
+
+	function count(store, item) {
+		let count = 0;
+
+		if (store?.length > 0) {
+			store.forEach((itemStore) => {
+				if (itemStore._id === item._id) {
+					count++;
+				}
+			});
+		}
+		return count;
+	}
+
+	const quantityNum = count(dataStore, item);
+
+	const [, dragRef] = useDrag({
+		type: 'ingr',
+		item: { item },
+		collect: (monitor) => {
+			return {
+				isDrag: monitor.isDragging(),
+			};
+		},
+	});
+
+	return (
+		<>
+			<li className={IngredientCss.ingredient} ref={dragRef}>
+				{quantityNum > 0 && <Counter count={quantityNum} size="default" extraClass="m-1" />}
+				<div>
+					<img src={image} alt={name} />
+				</div>
+				<div className={IngredientCss.textBox}>
+					<p>
+						<span className="text text_type_digits-default mb-1">{price}</span>
+						<CurrencyIcon type="primary" />
+					</p>
+				</div>
+				<p className={`text text_type_main-default ${IngredientCss.bottomText}`}>{name}</p>
+			</li>
+		</>
+	);
+};
+
+export default Ingredient;
---
+++ src/components/IngredientBox/IngredientBox.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { ItemConstructor } from '../../utils/types.tsx';
+import { forwardRef } from 'react';
+import { Link, useLocation } from 'react-router-dom';
+// import { IngredientType } from '../../utils/types';
+import IngredientBoxCss from './IngredientBoxCss.module.css';
+import Ingredient from '../Ingredient/Ingredient';
+
+interface Item {
+	_id: string;
+	image: string;
+	name: string;
+	price: number;
+}
+
+interface IngredientBoxProps {
+	dataStore: ItemConstructor[];
+	data: Item[];
+	title: string;
+	isModalOpen: boolean;
+	openModal: () => void;
+}
+
+const IngredientBox = forwardRef<HTMLDivElement, IngredientBoxProps>(({ dataStore, data, title, isModalOpen, openModal }, ref): React.JSX.Element => {
+	const location = useLocation();
+
+	return (
+		<div ref={ref} className={IngredientBoxCss.ingredientBox}>
+			<h3 className="text text_type_main-medium mb-6">{title}</h3>
+			<ul className={IngredientBoxCss.productsBox}>
+				{data?.length > 0 &&
+					data.map((item) => (
+						<Link className={IngredientBoxCss.link} key={item._id} to={`/ingredients/${item._id}`} state={{ background: location }}>
+							<Ingredient dataStore={dataStore} key={item._id} item={item} {...item} isModalOpen={isModalOpen} openModal={openModal} />
+						</Link>
+					))}
+			</ul>
+		</div>
+	);
+});
+
+IngredientBox.displayName = 'IngredientBox';
+
+export default IngredientBox;
