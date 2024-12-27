78ceffa3d464a9866aa88a67ac2e64a69fc8954e
s.potapov <s.potapov@severnaya.ru>
2024-12-27 10:03:31 +0300

    исправляет недочеты

--- src/components/FeedListOrders/FeedListOrders.tsx
+++ src/components/FeedListOrders/FeedListOrders.tsx
@@ -1,52 +1,54 @@
-import React from 'react';
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { connectFeed, disconnectFeed } from '../../services/actions/socket-action';
-import FeedListOrdersCss from './FeedListOrders.module.css';
-import FeedListOrder from '../FeedListOrder/FeedListOrder';
-import { OrderFeed } from '../../utils/types';
-import { FEED_SOCKET_URL_All } from '../../utils/vars';
+import React from "react";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { connectFeed, disconnectFeed } from "../../services/actions/socket-action";
+import FeedListOrdersCss from "./FeedListOrders.module.css";
+import FeedListOrder from "../FeedListOrder/FeedListOrder";
+import { OrderFeed } from "../../utils/types";
+import { FEED_SOCKET_URL_All } from "../../utils/vars";
 const MAX_ORDERS_COUNT = 5;
 
 const FeedListOrders = () => {
-	const dispatch = useDispatch();
-	const arrayAllOrders = useSelector((store) => store.feedReducer.orders.orders);
-	const isLoadingOrders = useSelector((store) => store.feedReducer.isLoading);
-	const slicedArrayOrders: OrderFeed[] = arrayAllOrders?.slice(0, MAX_ORDERS_COUNT);
+  const dispatch = useDispatch();
+  const arrayAllOrdersSocket = useSelector((store) => store.feedReducer.orders.orders);
+  const isLoadingOrders = useSelector((store) => store.feedReducer.isLoading);
+  const slicedArrayOrders: OrderFeed[] = arrayAllOrdersSocket?.slice(0, MAX_ORDERS_COUNT);
 
-	console.log(arrayAllOrders, '..arrayAllOrders...');
+  console.log(arrayAllOrdersSocket, "..arrayAllOrders...");
 
-	useEffect(() => {
-		// Подключение к WebSocket
-		dispatch(connectFeed(FEED_SOCKET_URL_All));
+  useEffect(() => {
+    // Подключение к WebSocket
+    dispatch(connectFeed(FEED_SOCKET_URL_All));
 
-		// Очистка при размонтировании компонента
-		return () => {
-			dispatch(disconnectFeed());
-		};
-	}, [dispatch]);
+    // Очистка при размонтировании компонента
+    return () => {
+      dispatch(disconnectFeed());
+    };
+  }, [dispatch]);
 
-	return (
-		<div>
-			{isLoadingOrders && <p className="text text_type_main-default">Заказы загружаются...</p>}
+  return (
+    <div>
+      {isLoadingOrders && <p className="text text_type_main-default">Заказы загружаются...</p>}
 
-			<ul className={FeedListOrdersCss.list}>
-				{slicedArrayOrders?.length > 0 &&
-					slicedArrayOrders.map((item) => (
-						<FeedListOrder
-							key={item._id}
-							_id={item._id}
-							ingredients={item.ingredients}
-							status={item.status}
-							name={item.name}
-							createdAt={item.createdAt}
-							updatedAt={item.updatedAt}
-							number={item.number}
-						/>
-					))}
-			</ul>
-		</div>
-	);
+      {!isLoadingOrders && (
+        <ul className={FeedListOrdersCss.list}>
+          {slicedArrayOrders?.length > 0 &&
+            slicedArrayOrders.map((item) => (
+              <FeedListOrder
+                key={item._id}
+                _id={item._id}
+                ingredients={item.ingredients}
+                status={item.status}
+                name={item.name}
+                createdAt={item.createdAt}
+                updatedAt={item.updatedAt}
+                number={item.number}
+              />
+            ))}
+        </ul>
+      )}
+    </div>
+  );
 };
 
 export default FeedListOrders;
