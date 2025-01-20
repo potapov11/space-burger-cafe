3ec894bea7a61a656040a36bbf46339d2888ca5d
Serezha <volos11223344@yandex.ru>
2025-01-20 22:42:49 +0300

    жобаил папку cypress

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'cypress';
 
 export default defineConfig({
 	e2e: {
-		supportFile: false, // Отключение файла поддержки
 		setupNodeEvents(on, config) {
 			// Реализуйте обработчики событий здесь
 		},
