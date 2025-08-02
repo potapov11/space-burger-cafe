ef74fd7bce9366bdcb2462eaf7082af05c67ac6d
Serezha <volos11223344@yandex.ru>
2025-08-02 20:33:45 +0300

    разбил на чанки

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,27 @@
-import { sentryVitePlugin } from "@sentry/vite-plugin";
+import { sentryVitePlugin } from '@sentry/vite-plugin';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
-    plugins: [react(), sentryVitePlugin({
-        org: "my-company-6bm",
-        project: "react-burger-app"
-    })],
+	plugins: [
+		react(),
+		sentryVitePlugin({
+			org: 'my-company-6bm',
+			project: 'react-burger-app',
+		}),
+	],
 
-    build: {
-        sourcemap: true
-    }
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, './src'),
+			'@assets': path.resolve(__dirname, './src/assets'),
+			'@components': path.resolve(__dirname, './src/components'),
+			'@pages': path.resolve(__dirname, './src/pages'),
+		},
+	},
+
+	build: {
+		sourcemap: true,
+	},
 });
